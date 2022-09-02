30e2878ca72101c57ce10039208f474cef7e0bfc
Tiến Trần <tientt@elcom.com.vn>
2022-09-02 23:13:14 +0700

    Learn RAW SQL Queries with laravel

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostsController;
+use Illuminate\Support\Facades\DB;
 
 /*
 |--------------------------------------------------------------------------
@@ -43,3 +44,32 @@ Route::get('/contact', [PostsController::class, 'contact']);
 Route::get('/post/contact/{id}/{name}/{passwords}', [PostsController::class, 'show_contact']);
 
 Route::resource('posts', PostsController::class);
+
+Route::get('/insert', function () {
+    DB::insert('insert into posts(title, content) values(?, ?)', ['PHP with Laravel','Laravel is the best thing that happened in PHP']);
+});
+
+Route::get('/read', function () {
+
+    $result = DB::select('select * from posts where id = ?', [1]);
+
+    return $result;
+
+    // foreach($result as $post) {
+    //     return $post->title;
+    // }
+
+});
+
+Route::get('/update', function () {
+
+    $update = DB::update('update posts set title = "update title" where id=?',[1]);
+
+    return $update;
+
+});
+
+Route::get('/delete', function () {
+    $delete = DB::delete('delete from posts where id=?',[2]);
+    return $delete;
+});
